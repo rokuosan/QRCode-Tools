512338fac2a94420feba33c02601a3872663f574
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-18 16:35:17 +0900

    戻り値を明示的に指定

--- src/main/kotlin/me/konso/qrcodeTools/qrcode/Generator.kt
+++ src/main/kotlin/me/konso/qrcodeTools/qrcode/Generator.kt
@@ -3,10 +3,11 @@ package me.konso.qrcodeTools.qrcode
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
+import java.awt.image.BufferedImage
 
 class Generator {
     private val writer = QRCodeWriter()
 
-    fun makeQRCode(text: String, width: Int = 512, height: Int = 512) =
+    fun makeQRCode(text: String, width: Int = 512, height: Int = 512): BufferedImage =
         MatrixToImageWriter.toBufferedImage(writer.encode(text, BarcodeFormat.QR_CODE, width, height))
 }
