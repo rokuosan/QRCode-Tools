32bd9ed07994cf31be46770071017c889d43408f
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-22 01:19:47 +0900

    GUIをComposeに変更

--- src/main/kotlin/me/konso/qrcodeTools/Main.kt
+++ src/main/kotlin/me/konso/qrcodeTools/Main.kt
@@ -1,7 +1,62 @@
 package me.konso.qrcodeTools
 
-import me.konso.qrcodeTools.window.MainWindow
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import me.konso.qrcodeTools.window.GeneratorWindow
 
-fun main(){
-    MainWindow()
+fun main() = application{
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "QRCode Tools",
+        state = rememberWindowState(width = 300.dp, height = 350.dp)
+    ){
+        MaterialTheme {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth()
+                    .then(Modifier.height(45.dp)),
+            ) {
+                Text("QRCode Tools")
+            }
+
+            Column(
+                Modifier.fillMaxSize().padding(PaddingValues(top=80.dp)),
+                horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                val buttons: Map<String, ()-> Unit> = mapOf(
+                    Store.OPEN_GENERATOR to { GeneratorWindow() },
+                    Store.OPEN_CAMERA to { println("camera") },
+                    Store.OPEN_CAPTURE to { println("capture") }
+                )
+
+                for(b in buttons){
+                    Button(
+                        onClick = {
+                            if(!Store.isOpenWindows[b.key]!!) b.value()
+
+                            Store.isOpenWindows += b.key to true
+                        },
+                        enabled = !Store.isOpenWindows[b.key]!!
+                    ){
+                        Text(b.key)
+                    }
+                }
+            }
+
+        }
+    }
 }
