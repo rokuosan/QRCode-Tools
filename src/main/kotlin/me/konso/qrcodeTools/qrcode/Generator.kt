0c4221470eaccc51f81c5f15725db582ae4ccfcf
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-21 22:46:08 +0900

    エンコード/デコードで使用する文字コードをUTF-8に変更した

--- src/main/kotlin/me/konso/qrcodeTools/qrcode/Generator.kt
+++ src/main/kotlin/me/konso/qrcodeTools/qrcode/Generator.kt
@@ -1,13 +1,22 @@
 package me.konso.qrcodeTools.qrcode
 
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import java.awt.image.BufferedImage
+import java.util.*
 
 object Generator {
     private val writer = QRCodeWriter()
 
-    fun makeQRCode(text: String, width: Int = 512, height: Int = 512): BufferedImage =
-        MatrixToImageWriter.toBufferedImage(writer.encode(text, BarcodeFormat.QR_CODE, width, height))
+    fun makeQRCode(text: String, width: Int = 512, height: Int = 512): BufferedImage{
+        val hints: MutableMap<EncodeHintType, Any> = EnumMap(EncodeHintType::class.java)
+        hints[EncodeHintType.ERROR_CORRECTION]=ErrorCorrectionLevel.L
+        hints[EncodeHintType.CHARACTER_SET]=Charsets.UTF_8.displayName()
+        hints[EncodeHintType.MARGIN]=4
+
+        return MatrixToImageWriter.toBufferedImage(writer.encode(text, BarcodeFormat.QR_CODE, width, height, hints))
+    }
 }
--- src/main/kotlin/me/konso/qrcodeTools/qrcode/Reader.kt
+++ src/main/kotlin/me/konso/qrcodeTools/qrcode/Reader.kt
@@ -2,10 +2,12 @@ package me.konso.qrcodeTools.qrcode
 
 import com.github.sarxos.webcam.Webcam
 import com.google.zxing.BinaryBitmap
+import com.google.zxing.DecodeHintType
 import com.google.zxing.MultiFormatReader
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import java.awt.image.BufferedImage
+import java.util.*
 
 object Reader{
     private lateinit var image: BufferedImage
@@ -24,9 +26,11 @@ object Reader{
         // Create bitmap
         val src = BufferedImageLuminanceSource(image)
         val bitmap = BinaryBitmap(HybridBinarizer(src))
+        val hints: MutableMap<DecodeHintType, Any> = EnumMap(DecodeHintType::class.java)
+        hints[DecodeHintType.CHARACTER_SET]=Charsets.UTF_8.displayName()
 
         return try{
-            MultiFormatReader().decode(bitmap).text
+            MultiFormatReader().decode(bitmap, hints).text
         }catch(e: Exception){
             null
         }
