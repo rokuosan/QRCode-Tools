86618d2b2c68736261288be9004225d6d8fec89c
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-22 09:20:05 +0900

    画面中央に表示されるようにした

--- src/main/kotlin/me/konso/qrcodeTools/Main.kt
+++ src/main/kotlin/me/konso/qrcodeTools/Main.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import me.konso.qrcodeTools.window.CaptureWindow
@@ -24,7 +25,10 @@ fun main() = application{
     Window(
         onCloseRequest = ::exitApplication,
         title = "QRCode Tools",
-        state = rememberWindowState(width = 300.dp, height = 350.dp)
+        state = rememberWindowState(
+            width = 300.dp, height = 350.dp,
+            position = WindowPosition(Alignment.Center)
+        )
     ){
         MaterialTheme {
             TopAppBar(
