0d2fc2f03149a141df37a1abffc2d6ba8e8084cb
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-10-23 22:11:56 +0900

    実装済みのボタンを追加

--- src/main/kotlin/me/konso/qrcodeTools/Main.kt
+++ src/main/kotlin/me/konso/qrcodeTools/Main.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -24,6 +25,7 @@ import me.konso.qrcodeTools.WindowType.DESKTOP
 import me.konso.qrcodeTools.WindowType.GENERATOR
 import me.konso.qrcodeTools.WindowType.values
 import me.konso.qrcodeTools.window.GeneratorWindow
+import me.konso.qrcodeTools.window.ReaderWindow
 
 enum class WindowType(
     val title: String,
@@ -34,7 +36,9 @@ enum class WindowType(
     DESKTOP("QR Code Reader [Desktop]", "Open Desktop Reader")
 }
 fun main() = application{
-    AppWindow(this)
+    CompositionLocalProvider(LocalAppResources provides rememberAppResources()){
+        AppWindow(this)
+    }
 }
 
 @Composable
@@ -64,6 +68,8 @@ fun AppWindow(app: ApplicationScope) {
             verticalAlignment = Alignment.CenterVertically,
         ) {
             for(w in windows) {
+                if(w==CAMERA) continue
+
                 Button(
                     onClick={ isOpen[w.name]=true },
                     enabled=!isOpen[w.name]!!
@@ -73,16 +79,37 @@ fun AppWindow(app: ApplicationScope) {
 
                 if(isOpen[w.name]!!) {
                     Window(
-                        onCloseRequest={ isOpen[w.name]=false },
+                        onCloseRequest={
+                            isOpen[w.name]=false
+                        },
                         title=w.title,
                         resizable=false
                     ) {
                         when(w){
                             GENERATOR -> GeneratorWindow()
                             DESKTOP -> TODO()
-                            CAMERA -> TODO()
+                            else -> {}
                         }
                     }
+                }
+            }
+
+            Button(
+                onClick={ isOpen[CAMERA.name]=true },
+                enabled=!isOpen[CAMERA.name]!!
+            ) {
+                Text(CAMERA.text)
+            }
+
+            if(isOpen[CAMERA.name]!!) {
+                Window(
+                    onCloseRequest={
+                        isOpen[CAMERA.name]=false
+                    },
+                    title=CAMERA.title,
+                    resizable=false,
+                ) {
+                    ReaderWindow()
                 }
             }
         }
